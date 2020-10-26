0473a3c7f85c1da2222dbcdccbf38ee1931c3550
Alexandre Boyer <alexandre.boyer@orange.fr>
2020-10-26 11:20:20 +0400

    Ajout du listener sur le bouton du MainActivity

--- app/src/main/java/com/example/soukousschallenge/MainActivity.java
+++ app/src/main/java/com/example/soukousschallenge/MainActivity.java
@@ -3,13 +3,33 @@ package com.example.soukousschallenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TableLayout mBestScoresTable;
+    private TextView mBestScoresLabel;
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Ceci est un test");
+
+        mBestScoresTable = findViewById(R.id.activity_main_bestScoresTable);
+        mBestScoresLabel = findViewById(R.id.activity_main_bestScoresLabel);
+        mStartButton = findViewById(R.id.activity_main_startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lancement de la vue de jeu
+                System.out.println("Une nouvelle partie se lance !");
+            }
+        });
+
+
     }
 }
