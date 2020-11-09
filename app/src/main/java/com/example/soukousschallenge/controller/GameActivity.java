2a4ce4f6e7356d16e0df3c08f024996a276b3331
David Boisedu <david.boisedu@live.fr>
2020-11-09 16:53:15 +0400

    Ajout de l'ID du bouton Pause + écouteur dans la GameActivity

--- app/src/main/java/com/example/soukousschallenge/controller/GameActivity.java
+++ app/src/main/java/com/example/soukousschallenge/controller/GameActivity.java
@@ -3,14 +3,29 @@ package com.example.soukousschallenge.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.soukousschallenge.R;
 
 public class GameActivity extends AppCompatActivity {
 
+    private ImageButton mPauseButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mPauseButton.findViewById(R.id.activity_game_pauseButton);
+
+        mPauseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //code à implémenter
+            }
+        });
     }
+
+
 }
