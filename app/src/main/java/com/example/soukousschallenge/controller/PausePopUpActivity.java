cf6603eb1c824be42754c81e24a390b2d2dce10e
Alexandre Boyer <alexandre.boyer@orange.fr>
2020-11-09 21:13:43 +0400

    Implementation de PausePopUpActivity

---
+++ app/src/main/java/com/example/soukousschallenge/controller/PausePopUpActivity.java
@@ -0,0 +1,48 @@
+package com.example.soukousschallenge.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.soukousschallenge.R;
+
+public class PausePopUpActivity extends AppCompatActivity {
+
+    private Button mQuitButton;
+    private Button mResumeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pause_pop_up);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getRealMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*0.8),(int) (height*0.6));
+
+        mQuitButton = findViewById(R.id.popupButtonQuit);
+        mResumeButton = findViewById(R.id.popupButtonResume);
+
+        mQuitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+
+        mResumeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
