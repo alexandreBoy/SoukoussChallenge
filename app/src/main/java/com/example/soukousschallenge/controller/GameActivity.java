91e31140b603369ceebe093923f5d7df73a0f5d5
Alexandre Boyer <alexandre.boyer@orange.fr>
2020-11-13 10:46:20 +0400

    Fix compatibilite SHAKE et TURN OVER

--- app/src/main/java/com/example/soukousschallenge/controller/GameActivity.java
+++ app/src/main/java/com/example/soukousschallenge/controller/GameActivity.java
@@ -22,6 +22,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
     public static final int PAUSE_POPUP = 1;
     private static final String TAG = "DETECTION";
     private SensorManager mSensorManager;
+    private Sensor mAccelerometerSensor;
     private Sensor mGravitySensor;
     private boolean mFacingDown;
 
@@ -41,6 +42,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
 
         mPauseButton = findViewById(R.id.activity_game_pauseButton);
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mGravitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 
         if(mGravitySensor == null){
@@ -59,28 +61,29 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (mGravitySensor != null) {
-            mSensorManager.registerListener(this, mGravitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    public void onSensorChanged(SensorEvent event){
+
+        // ################ DETECTION ######################
+        // ################# SHAKE ########################
+        if (event.sensor == mAccelerometerSensor)
+        {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            if (mAccel > 12) {
+                Log.i(TAG, "Soukouss");
+            }
         }
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent){
-        /*
-        ################ DETECTION ######################
-        ################# TURN OVER #####################
+        // ################################################
+        // ################# TURN OVER #####################
         final float factor = 0.95F;
 
-        if (sensorEvent.sensor == mGravitySensor) {
-            boolean nowDown = sensorEvent.values[2] < -SensorManager.GRAVITY_EARTH * factor;
+        if (event.sensor == mGravitySensor) {
+            boolean nowDown = event.values[2] < -SensorManager.GRAVITY_EARTH * factor;
             if (nowDown != mFacingDown) {
                 if (nowDown) {
                     Log.i(TAG, "DOWN");
@@ -90,24 +93,29 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
                 mFacingDown = nowDown;
             }
         }
-        ################################################
-        ################# SHAKE ########################
-        float x = event.values[0];
-        float y = event.values[1];
-        float z = event.values[2];
-        mAccelLast = mAccelCurrent;
-        mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
-        float delta = mAccelCurrent - mAccelLast;
-        mAccel = mAccel * 0.9f + delta;
-        if (mAccel > 12) {
-            Log.i(TAG, "Soukouss");
-        }
-        ################################################
-        */
+        // ################################################
+
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    protected void onResume() {
+
+        if (mGravitySensor != null) {
+            mSensorManager.registerListener(this, mGravitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        mSensorManager.unregisterListener(this);
+        super.onPause();
     }
 }
