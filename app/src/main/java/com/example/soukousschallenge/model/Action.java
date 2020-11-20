443c4cc488c2779140fb3e886c5b2254f07eff2e
Alexandre Boyer <alexandre.boyer@orange.fr>
2020-11-20 15:01:03 +0400

    Modif getter/setter classe Action et centrage label score

--- app/src/main/java/com/example/soukousschallenge/model/Action.java
+++ app/src/main/java/com/example/soukousschallenge/model/Action.java
@@ -1,18 +1,18 @@
 package com.example.soukousschallenge.model;
 
 public abstract class Action{
-    boolean terminated;
+    boolean selected;
     int ID;
 
     public abstract boolean perform();
 
     // Getters & Setters
-    public boolean isTerminated(){
-        return terminated;
+    public boolean isSelected(){
+        return selected;
     }
 
-    public void setTerminated(boolean terminated){
-        this.terminated = terminated;
+    public void setSelected(boolean selected){
+        this.selected = selected;
     }
 
     public int getID() { return ID; }
